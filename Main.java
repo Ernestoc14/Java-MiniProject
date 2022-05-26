343800b80094d26ad89f8160c82ca81b9659e7d6
ernestoc14 <descreed34@hotmail.com>
2022-05-26 15:15:27 -0500

    Add Main

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int opc;
+        opc = 0;
+        
+        switch (opc) {
+            case 1: //code
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
